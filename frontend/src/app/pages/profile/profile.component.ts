19242dfc982cf3a873775c075e444782cda0fa8c
Khoin06 <ngokhoinsl@gmail.com>
2025-11-16 00:08:53 -0800

    update ui frofile

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -7,7 +7,8 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-profile',
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
   user: any;
