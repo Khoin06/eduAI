8ff183213014bc29c3ac72c05e6b11888bc82de8
Khoin06 <ngokhoinsl@gmail.com>
2025-11-15 19:25:26 -0800

    update ui

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,7 +11,8 @@ import { AuthService } from './services/auth.service';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SidebarComponent, HeaderComponent,CommonModule],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   isLoggedIn = false;
